0eeb3e8620f367ed00840168194619f958a31d8a
lukaszjargielo <l.jargielo88@gmail.com>
2025-03-14 10:32:05 +0100

    Skeleton of application

---
+++ src/test/java/com/alansystems/footballstatistics/FootballStatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alansystems.footballstatistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballStatisticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
