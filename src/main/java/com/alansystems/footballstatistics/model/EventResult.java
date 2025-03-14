f0e192b5fe862260dfe389e50680be91c921a6a2
lukaszjargielo <l.jargielo88@gmail.com>
2025-03-14 13:23:20 +0100

    Add EventResult class

---
+++ src/main/java/com/alansystems/footballstatistics/model/EventResult.java
@@ -0,0 +1,31 @@
+package com.alansystems.footballstatistics.model;
+
+public class EventResult {
+    private final String homeTeam;
+    private final String awayTeam;
+    private final int homeScore;
+    private final int awayScore;
+
+    public EventResult(String homeTeam, String awayTeam, int homeScore, int awayScore) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeScore = homeScore;
+        this.awayScore = awayScore;
+    }
+
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+
+    public String getAwayTeam() {
+        return awayTeam;
+    }
+
+    public int getHomeScore() {
+        return homeScore;
+    }
+
+    public int getAwayScore() {
+        return awayScore;
+    }
+}
