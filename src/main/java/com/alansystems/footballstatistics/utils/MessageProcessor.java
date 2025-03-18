16c4181fb38a6e8e154580f788fbbf1c0d2aaecd
lukaszjargielo <l.jargielo88@gmail.com>
2025-03-18 14:54:00 +0100

    Separation of 6 methods from one big processMessage() method

--- src/main/java/com/alansystems/footballstatistics/utils/MessageProcessor.java
+++ src/main/java/com/alansystems/footballstatistics/utils/MessageProcessor.java
@@ -17,115 +17,102 @@ public class MessageProcessor {
     public static void processMessage(List<Message> messages) {
         for (Message message : messages) {
             if (message.getType().equals(MessageType.RESULT)) {
-                EventResult eventResult = message.getResult();
-
-                String homeTeam = eventResult.getHomeTeam();
-                String awayTeam = eventResult.getAwayTeam();
-                int homeScore = eventResult.getHomeScore();
-                int awayScore = eventResult.getAwayScore();
-
-                TeamStatistics homeTeamObj;
-                TeamStatistics awayTeamObj;
-
-                if (!mapWithTeamStatistics.containsKey(homeTeam)) {
-                    mapWithTeamStatistics.put(homeTeam,
-                            new TeamStatistics(homeTeam,
-                                    null,
-                                    null,
-                                    null,
-                                    0.0,
-                                    1,
-                                    0,
-                                    homeScore,
-                                    awayScore));
-                    homeTeamObj = mapWithTeamStatistics.get(homeTeam);
-                } else {
-                    homeTeamObj = mapWithTeamStatistics.get(homeTeam);
-
-                    homeTeamObj.setThirdLastMatchResult(homeTeamObj.getSecondLastMatchResult());
-                    homeTeamObj.setSecondLastMatchResult(homeTeamObj.getLastMatchResult());
-
-                    homeTeamObj.setNumberOfPlayedEvents(homeTeamObj.getNumberOfPlayedEvents() + 1);
-
-                    homeTeamObj.setSumOfGoalsScored(homeTeamObj.getSumOfGoalsScored() + homeScore);
-                    homeTeamObj.setSumOfGoalsConceded(homeTeamObj.getSumOfGoalsConceded() + awayScore);
-                }
-
-                if (!mapWithTeamStatistics.containsKey(awayTeam)) {
-                    mapWithTeamStatistics.put(awayTeam,
-                            new TeamStatistics(awayTeam,
-                                    null,
-                                    null,
-                                    null,
-                                    0.0,
-                                    1,
-                                    0,
-                                    awayScore,
-                                    homeScore));
-                    awayTeamObj = mapWithTeamStatistics.get(awayTeam);
-                } else {
-                    awayTeamObj = mapWithTeamStatistics.get(awayTeam);
-
-                    awayTeamObj.setThirdLastMatchResult(awayTeamObj.getSecondLastMatchResult());
-                    awayTeamObj.setSecondLastMatchResult(awayTeamObj.getLastMatchResult());
-
-                    awayTeamObj.setNumberOfPlayedEvents(awayTeamObj.getNumberOfPlayedEvents() + 1);
-
-                    awayTeamObj.setSumOfGoalsScored(awayTeamObj.getSumOfGoalsScored() + awayScore);
-                    awayTeamObj.setSumOfGoalsConceded(awayTeamObj.getSumOfGoalsConceded() + homeScore);
-                }
-
-                int homeTeamGainedPoints = 0;
-                int awayTeamGainedPoints = 0;
-
-                EventStatuses homeTeamLastEventStatus = null;
-                EventStatuses awayTeamLastEventStatus = null;
-
-                if (homeScore == awayScore) {
-                    awayTeamGainedPoints++;
-                    homeTeamGainedPoints++;
-
-                    homeTeamLastEventStatus = EventStatuses.D;
-                    awayTeamLastEventStatus = EventStatuses.D;
-
-                } else if (homeScore > awayScore) {
-                    homeTeamGainedPoints += 3;
-
-                    homeTeamLastEventStatus = EventStatuses.W;
-                    awayTeamLastEventStatus = EventStatuses.L;
-                } else {
-                    awayTeamGainedPoints += 3;
-
-                    homeTeamLastEventStatus = EventStatuses.L;
-                    awayTeamLastEventStatus = EventStatuses.W;
-                }
-
-                homeTeamObj.setLastMatchResult(homeTeamLastEventStatus);
-                awayTeamObj.setLastMatchResult(awayTeamLastEventStatus);
-
-                homeTeamObj.setSumOfGainedPoints(homeTeamObj.getSumOfGainedPoints() + homeTeamGainedPoints);
-                awayTeamObj.setSumOfGainedPoints(awayTeamObj.getSumOfGainedPoints() + awayTeamGainedPoints);
-
-                double homeTeamAverageAmountOfGoalsInAllEvents;
-                homeTeamAverageAmountOfGoalsInAllEvents = Math.round(((homeTeamObj.getSumOfGoalsScored() + homeTeamObj.getSumOfGoalsConceded()) * 1.0 / homeTeamObj.getNumberOfPlayedEvents()) * 100.0) / 100.0;
-                homeTeamObj.setAverageAmountOfGoalsInTheTeamEvents(homeTeamAverageAmountOfGoalsInAllEvents);
-
-                double awayTeamAverageAmountOfGoalsInAllEvents;
-                awayTeamAverageAmountOfGoalsInAllEvents = Math.round(((awayTeamObj.getSumOfGoalsScored() + awayTeamObj.getSumOfGoalsConceded()) * 1.0 / awayTeamObj.getNumberOfPlayedEvents()) * 100.0) / 100.0;
-                awayTeamObj.setAverageAmountOfGoalsInTheTeamEvents(awayTeamAverageAmountOfGoalsInAllEvents);
-
-                printSimpleStatistics(homeTeamObj);
-                printSimpleStatistics(awayTeamObj);
-                System.out.println();
-
+                handleResultMessage(message.getResult());
             } else if (message.getType().equals(MessageType.GET_STATISTICS)) {
-                TeamListForStatistics teamListForStatistics = message.getGetStatistics();
-                List<String> teams = teamListForStatistics.getTeams();
-                for (String team : teams) {
-                    printAdvancedStatistics(mapWithTeamStatistics.get(team));
-                }
-                System.out.println();
+                handleGetStatisticsMessage(message.getGetStatistics());
             }
         }
     }
+
+    private static void handleResultMessage(EventResult eventResult) {
+        String homeTeam = eventResult.getHomeTeam();
+        String awayTeam = eventResult.getAwayTeam();
+        int homeScore = eventResult.getHomeScore();
+        int awayScore = eventResult.getAwayScore();
+
+        TeamStatistics homeTeamObj = processTeam(homeTeam, homeScore, awayScore, true);
+        TeamStatistics awayTeamObj = processTeam(awayTeam, awayScore, homeScore, false);
+
+        updatePointsAndStatus(homeTeamObj, awayTeamObj, homeScore, awayScore);
+
+        updateAverageGoals(homeTeamObj);
+        updateAverageGoals(awayTeamObj);
+
+        printSimpleStatistics(homeTeamObj);
+        printSimpleStatistics(awayTeamObj);
+        System.out.println();
+    }
+
+    private static void handleGetStatisticsMessage(TeamListForStatistics teamListForStatistics) {
+        List<String> teams = teamListForStatistics.getTeams();
+        for (String team : teams) {
+            printAdvancedStatistics(mapWithTeamStatistics.get(team));
+        }
+        System.out.println();
+    }
+
+    private static TeamStatistics processTeam(String teamName, int goalsScored, int goalsConceded, boolean isHomeTeam) {
+        TeamStatistics teamObj;
+
+        if (!mapWithTeamStatistics.containsKey(teamName)) {
+            teamObj = new TeamStatistics(teamName,
+                    null,
+                    null,
+                    null,
+                    0.0,
+                    1,
+                    0,
+                    goalsScored,
+                    goalsConceded);
+            mapWithTeamStatistics.put(teamName, teamObj);
+        } else {
+            teamObj = mapWithTeamStatistics.get(teamName);
+            updateTeamStatistics(teamObj, goalsScored, goalsConceded);
+        }
+        return teamObj;
+    }
+
+    private static void updatePointsAndStatus(TeamStatistics homeTeamObj, TeamStatistics awayTeamObj, int homeScore,
+                                              int awayScore) {
+        int homeTeamGainedPoints = 0;
+        int awayTeamGainedPoints = 0;
+
+        EventStatuses homeTeamLastEventStatus = null;
+        EventStatuses awayTeamLastEventStatus = null;
+
+        if (homeScore == awayScore) {
+            homeTeamGainedPoints++;
+            awayTeamGainedPoints++;
+            homeTeamLastEventStatus = EventStatuses.D;
+            awayTeamLastEventStatus = EventStatuses.D;
+        } else if (homeScore > awayScore) {
+            homeTeamGainedPoints += 3;
+            homeTeamLastEventStatus = EventStatuses.W;
+            awayTeamLastEventStatus = EventStatuses.L;
+        } else {
+            awayTeamGainedPoints += 3;
+            homeTeamLastEventStatus = EventStatuses.L;
+            awayTeamLastEventStatus = EventStatuses.W;
+        }
+
+        homeTeamObj.setLastMatchResult(homeTeamLastEventStatus);
+        awayTeamObj.setLastMatchResult(awayTeamLastEventStatus);
+
+        homeTeamObj.setSumOfGainedPoints(homeTeamObj.getSumOfGainedPoints() + homeTeamGainedPoints);
+        awayTeamObj.setSumOfGainedPoints(awayTeamObj.getSumOfGainedPoints() + awayTeamGainedPoints);
+    }
+
+    private static void updateAverageGoals(TeamStatistics teamObj) {
+        double averageGoals = Math.round(((teamObj.getSumOfGoalsScored() + teamObj.getSumOfGoalsConceded()) * 1.0 / teamObj.getNumberOfPlayedEvents()) * 100.0) / 100.0;
+        teamObj.setAverageAmountOfGoalsInTheTeamEvents(averageGoals);
+    }
+
+    private static void updateTeamStatistics(TeamStatistics teamObj, int goalsScored, int goalsConceded) {
+        teamObj.setThirdLastMatchResult(teamObj.getSecondLastMatchResult());
+        teamObj.setSecondLastMatchResult(teamObj.getLastMatchResult());
+        teamObj.setNumberOfPlayedEvents(teamObj.getNumberOfPlayedEvents() + 1);
+        teamObj.setSumOfGoalsScored(teamObj.getSumOfGoalsScored() + goalsScored);
+        teamObj.setSumOfGoalsConceded(teamObj.getSumOfGoalsConceded() + goalsConceded);
+    }
+
 }
